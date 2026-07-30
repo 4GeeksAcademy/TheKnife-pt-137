777851404a4c8eb79eae18dc50080066e5d436c7
Juan Donday Rodríguez <jdondayr@gmail.com>
2026-07-30 16:48:15 +0000

    Waiter relationships: relation between tables, endpoint logic modifications, and react components modification

--- src/api/models.py
+++ src/api/models.py
@@ -24,6 +24,7 @@ class Restaurant(db.Model):
     # Relationships
     products: Mapped[list["Product"]] = relationship(back_populates="restaurant")
     chef: Mapped["Chef"] = relationship(back_populates="restaurant")
+    waiters: Mapped[list["Waiter"]] = relationship(back_populates="restaurant")
 
     def serialize(self):
         return {
@@ -45,13 +46,17 @@ class Waiter(db.Model):
     name: Mapped[str] = mapped_column(String(20), nullable=False)
     email: Mapped[str] = mapped_column(String(30), nullable=False)
     password: Mapped[str] = mapped_column(String(255), nullable=False)
-    ## aquí falta el restaurant_id cuando lo haagamos con relaciones
+    restaurant_id: Mapped[int] = mapped_column(ForeignKey("restaurant.id"))
+
+    # Relationships
+    restaurant: Mapped["Restaurant"] = relationship(back_populates="waiters")
 
     def serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "email": self.email
+            "email": self.email,
+            "restaurant_id": self.restaurant_id
         }
 
 ## Chef
