f71fa331185eed0b7a9671cdc48b001aeef1e3d2
Juan Donday Rodríguez <jdondayr@gmail.com>
2026-08-12 08:09:30 +0000

    fixed centered of map

--- src/front/pages/restaurant/actions/ClientSearchNearbyRestaurants.jsx
+++ src/front/pages/restaurant/actions/ClientSearchNearbyRestaurants.jsx
@@ -80,7 +80,7 @@ const ClientSearchNearbyRestaurants = () => {
             </div>
 
             <div style={{ width: "100%", height: "500px" }}>
-                <Map defaultCenter={center} defaultZoom={6} mapId="DEMO_MAP_ID" onClick={(e) => setStartPointData({ ...startPointData, latitude: e.detail.latLng.lat, longitude: e.detail.latLng.lng })}>
+                <Map defaultCenter={center} defaultZoom={7} mapId="DEMO_MAP_ID" onClick={(e) => setStartPointData({ ...startPointData, latitude: e.detail.latLng.lat, longitude: e.detail.latLng.lng })}>
                     {startPointData.latitude && startPointData.longitude ?
                         <AdvancedMarker
                             draggable={true}
